e7e27ebfeefdb44c609c66c11dbd16b2c5cbaa6c
Anna Makarudze <remote@DESKTOP-G1U5ECB.localdomain>
2018-11-23 22:40:35 +0200

    Add auth views

--- feature_requests/auth.py
+++ feature_requests/auth.py
@@ -1,8 +1,85 @@
 import functools
 
-from flask import Blueprint, flash, g, render_template, request, session, url_for
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from .models import db
+from .models import db, User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        error = None
+
+    if not username:
+        error = 'Username is required.'
+    elif not email:
+        error = 'Email is required.'
+    elif not password:
+        error = 'Password is required.'
+    elif User.query.filter_by(username=username).first() is not None:
+        error = 'Username {} already registered.'
+
+    if error is None:
+        user = User(username=username, email=email, password=generate_password_hash(password))
+        db.session.add(user)
+        return redirect(url_for('auth.login'))
+
+    flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        user = User.query.filter_by(username=username)
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect username/password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.filter_by(user_id=user_id)
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.login'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
